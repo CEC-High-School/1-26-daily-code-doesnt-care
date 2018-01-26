9852b2850511d4d5ddda27bca01c98d5385eb58a
FatalPanic1035 <31856984+FatalPanic1035@users.noreply.github.com>
2018-01-26 09:07:33 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,48 @@
+#include <allegro5\allegro.h>
+#include <allegro5\allegro_image.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	srand(time(NULL));
+	ALLEGRO_DISPLAY *gamescreen = NULL;
+	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+	ALLEGRO_TIMER *timer = NULL;
+	ALLEGRO_BITMAP *images = NULL;
+	
+	int a, b, c;
+
+	a = rand() % 500;
+	b = rand() % 200;
+	c = rand() % 700;
 
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+	al_init();
+
+	al_init_image_addon();
+
+	al_create_timer(5);
+	
+	gamescreen = al_create_display(500,500);
+	
+	al_set_target_backbuffer(gamescreen);
+
+	event_queue = al_create_event_queue();
+
+	al_clear_to_color(al_map_rgb(a, b, c));
+
+	al_flip_display();
+
+	al_register_event_source(event_queue, al_get_display_event_source(gamescreen));
+
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+
+	al_start_timer(timer);
+
+
+	al_destroy_timer(timer);
+	al_destroy_display(gamescreen);
+	al_destroy_event_queue(event_queue);
+
+	return 0;
+}
